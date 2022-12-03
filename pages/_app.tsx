edddd515649234c98a93189a17de822d34766d38
Swapneel Bobba <swapneelbobba@Swapneels-MacBook-Air.local>
2022-12-03 08:54:15 +0530

    setup chakraui + initial homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import {ChakraProvider} from '@chakra-ui/react';
+import {extendTheme} from '@chakra-ui/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const colors = {
+  brand: {
+    900: '#1d4ed8',
+    800: '#2563eb',
+    700: '#3b82f6',
+  },
+};
+
+const theme = extendTheme({colors});
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
