59b9799998cdca05f26c0be7d2513f670b928808
Swapneel Bobba <swapneelbobba@Swapneels-MacBook-Air.local>
2022-12-03 08:22:38 +0530

    first commit: next, tailwind, typescript setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-3xl font-bold underline text-red-300">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
